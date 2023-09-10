fc9e70257615d0a6eff22e8079ec5d56119e47c3
OKUNADE <yachur@ymail.com>
2023-09-10 10:05:28 +0100

    add brochure box in service-template page to include it

---
+++ moriahCareServices/resources/views/include/brochureBox.blade.php
@@ -0,0 +1,9 @@
+   <!--Brochures Box-->
+ <div class="brochures-box">
+                            <div class="inner">
+                                <h4>Download Brochures</h4>
+                                <div class="text">Pleasure and praising pain was born and I will give you a complete account.</div>
+                                <a class="theme-btn btn-style-one" href="#"><span class="btn-title"><i class="fa fa-file-pdf"></i> Info Company</span></a>
+                                <a class="theme-btn btn-style-one" href="#"><span class="btn-title"><i class="fa fa-file-pdf"></i> Brochure Newest</span></a>
+                            </div>
+                        </div>
--- moriahCareServices/resources/views/pages/service-template.blade.php
+++ moriahCareServices/resources/views/pages/service-template.blade.php
@@ -76,17 +76,10 @@
                     <aside class="sidebar services-sidebar">
                         
                         <!-- Service Widget -->
-                        @include('include.servicewidget')
+                        @include('include.serviceWidget')
                         
                         <!--Brochures Box-->
-                        <div class="brochures-box">
-                            <div class="inner">
-                                <h4>Download Brochures</h4>
-                                <div class="text">Pleasure and praising pain was born and I will give you a complete account.</div>
-                                <a class="theme-btn btn-style-one" href="#"><span class="btn-title"><i class="fa fa-file-pdf"></i> Info Company</span></a>
-                                <a class="theme-btn btn-style-one" href="#"><span class="btn-title"><i class="fa fa-file-pdf"></i> Brochure Newest</span></a>
-                            </div>
-                        </div>
+                      @include('include.brochureBox')
                     </aside>
                 </div>
                <!-- /leftside -->
